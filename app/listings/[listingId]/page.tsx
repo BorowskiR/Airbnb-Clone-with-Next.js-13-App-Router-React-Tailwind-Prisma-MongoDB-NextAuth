08e7a7dd9dfb4a90d31158e448b5e452b7ca4ac2
Rafal <52796317+BorowskiR@users.noreply.github.com>
2023-06-06 22:52:55 +0200

    getReservation action

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -2,6 +2,7 @@ import getCurrentUser from '@/actions/getCurrentUser';
 import getListingById from '@/actions/getListingById';
 import { EmptyState } from '@/components/EmptyState';
 import ListingClient from './ListingClient';
+import getReservations from '@/actions/getReservations';
 
 interface IParams {
   listingId: string;
@@ -9,11 +10,18 @@ interface IParams {
 
 export default async function ListingPage({ params }: { params: IParams }) {
   const listing = await getListingById(params);
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
     return <EmptyState />;
   }
 
-  return <ListingClient listing={listing} currentUser={currentUser} />;
+  return (
+    <ListingClient
+      reservations={reservations}
+      listing={listing}
+      currentUser={currentUser}
+    />
+  );
 }
