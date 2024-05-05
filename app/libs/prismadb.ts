9840c98d273a489c3c7fd8e95481dede109300bd
Rafal Borowski <rafall.borowski@gmail.com>
2024-05-05 19:07:45 +0200

    prisma fix v2

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -14,7 +14,9 @@ if (typeof window === 'undefined') {
   if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
   } else {
+    // @ts-ignore
     if (!global.prisma) {
+      // @ts-ignore
       global.prisma = new PrismaClient();
     }
 
